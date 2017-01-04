f2867a63666d67e0bd96d392badba890f763f0c4
zoltanforis <foris64@gmail.com>
2017-01-04 06:37:49 +0100

    Add files via upload
    input file read capability added

--- natlan3.py
+++ natlan3.py
@@ -1,10 +1,10 @@
-import sys, gl, conc, wrd
+import sys, gl, conc, wrd, testing
 
 
-gl.args=gl.Arguments()                #initialize
-gl.WM = conc.Kbase("WM")                        # WORKING MEMORY
-gl.KB = conc.Kbase("KB")                        # KNOWLEDGE BASE
-gl.WL = wrd.Wlist("WL")                        # WORD LIST
+gl.args=gl.Arguments()          #initialize
+gl.WM = conc.Kbase("WM")        # WORKING MEMORY
+gl.KB = conc.Kbase("KB")        # KNOWLEDGE BASE
+gl.WL = wrd.Wlist("WL")         # WORD LIST
 gl.log = gl.Logging()
 
 gl.KB.add_concept(3,3,[9,5])
@@ -16,6 +16,12 @@ gl.WL.add_word("joska")
 test1=conc.Concept()
 test1.add_parents([-1,17])
 result=gl.WM.search_inlist(test1)
+
+if gl.args.argnum==2:
+    gl.test=testing.Testinput(sys.argv[1])
+    gl.test.readtest()
+    print ("input file read test - row 3 eng:",gl.test.eng[2],"row 5 mentalese:",gl.test.mentalese[4],"goodanswer:",gl.test.goodanswer[4])
+    gl.test.testf.close()
 
 gl.log.logf.close()
 print (gl.KB.cp[0].parent,"word with index 1=",gl.WL.wcp[1].word,"search:",result)
---
+++ testing.py
@@ -0,0 +1,48 @@
+import gl
+
+class Testinput:
+    def __init__(self,testfilename):
+        self.name=testfilename
+        self.eng = []               #english text
+        self.mentalese = []         #mentalese version of english
+        self.goodanswer = []        #expected answer (if english is a question)
+        self.question = []          #flag to indicate questions
+        self.systemanswer = []      #output of the system
+        self.evaluation = []        #evaluation of system output includinmg mentalese translation
+        try:
+            self.testf = open(testfilename,"r")
+        except: gl.log.add_log(("ERROR: Testinput: the file could not be opened:",self.name))
+    def readtest(self):
+        gl.log.add_log(("Test file started:",self.name))
+        for line in self.testf:
+            i=0;epos=-1;mpos=-1;apos=-1;comment=-1
+            self.eng.append(" "); self.mentalese.append(" ")            #each list must have a new item
+            self.goodanswer.append(" "); self.question.append(0)
+            self.systemanswer.append(" "); self.evaluation.append(" ")
+            rowi=len(self.eng)-1                                        #index of the new item
+            while i<len(line):
+                if "e/" in line[i:i+2]:epos=i                           #order of e/ m/ a/ // is fixed but all are optioonal
+                if "m/" in line[i:i+2]:
+                    mpos=i
+                    if (epos>-1): self.eng[rowi]=line[epos+2:mpos]
+                if "a/" in line[i:i+2]:
+                    apos=i
+                    self.question[rowi]=1
+                    if (mpos>-1): self.mentalese[rowi]=line[mpos+2:apos]
+                    if (mpos==-1 and epos>-1): self.eng[rowi]=line[epos+2:apos]
+                if "//" in line[i:i+2]:
+                    comment=i
+                    if (epos>-1 and mpos==-1): self.eng[rowi]=line[epos+2:comment]
+                    if (mpos>-1 and apos==-1): self.mentalese[rowi]=line[mpos+2:comment]
+                    if (apos>-1): self.goodanswer[rowi]=line[apos+2:comment]
+                i=i+1
+            if (epos>-1 and mpos==-1 and apos==-1 and comment==-1):
+                if ("\n" in line[i-2:i]): self.eng[rowi]=line[epos+2:i-2]
+                else: self.eng[rowi]=line[epos+2:i]
+            if (mpos>-1 and apos==-1 and comment==-1):
+                if ("\n" in line[i-2:i]): self.mentalese[rowi]=line[mpos+2:i-2]
+                else: self.mentalese[rowi]=line[mpos+2:i]
+            if (apos>-1 and comment==-1):
+                self.question[rowi]=1
+                if ("\n" in line[i-2:i]): self.goodanswer[rowi]=line[apos+2:i-2]
+                else: self.goodanswer[rowi]=line[apos+2:i]
