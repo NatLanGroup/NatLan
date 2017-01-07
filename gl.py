2b341f7c920b8ef040d903804982b17f83ac1598
Gabor Nagypal <nagypalg@gmail.com>
2017-01-07 17:52:20 +0100

    reformatted files, fixed warnings from Python IDE

--- gl.py
+++ gl.py
@@ -1,38 +1,39 @@
-
 import sys
 
-#This is a "glue" module
+
+# This is a "glue" module
 
 class Arguments:
-    
     def __init__(self):
         self.argnum = len(sys.argv)
-        
-        self.pgranu = 4                           # granularity for p, pgranu+1 discrete values from 0
-        
-        #pclas is the matrix for class reasoning. C(%1,%2)p1 and %X(%2,%3)p2 -> %x(%2,%3)pclas, pclas[p2,p1]
-        self.pclas = [[0,0,0,0,0],[0,1,1,1,1],[0,1,1,2,2],[0,1,2,2,3],[0,1,2,3,4]]     
+
+        self.pgranu = 4  # granularity for p, pgranu+1 discrete values from 0
+
+        # pclas is the matrix for class reasoning. C(%1,%2)p1 and %X(%2,%3)p2 -> %x(%2,%3)pclas, pclas[p2,p1]
+        self.pclas = [[0, 0, 0, 0, 0], [0, 1, 1, 1, 1], [0, 1, 1, 2, 2], [0, 1, 2, 2, 3], [0, 1, 2, 3, 4]]
 
         self.relationcode = {
-            "W":1, "S":2, "D":3, "C":4, "F":5,
-            "Q":6, "A":7, "I":8, "R":9, "T":10,
-            "P":11, "M":12, "IM":13, "N":14, "V":15,
-            "AND":16, "NOT":17, "OR":18, "XOR":19
+            "W": 1, "S": 2, "D": 3, "C": 4, "F": 5,
+            "Q": 6, "A": 7, "I": 8, "R": 9, "T": 10,
+            "P": 11, "M": 12, "IM": 13, "N": 14, "V": 15,
+            "AND": 16, "NOT": 17, "OR": 18, "XOR": 19
         }
 
+
 class Logging:
-    
-    def __init__(self,fname="logfile.txt"):
-        try: self.logf=open(fname,"w")
-        except: print("ERROR: Logging: log file could not be opened")
-    
-    def add_log(self,what):                                 #what must be iterable
+    def __init__(self, fname="logfile.txt"):
+        try:
+            self.logf = open(fname, "w")
+        except:
+            print("ERROR: Logging: log file could not be opened")
+
+    def add_log(self, what):  # what must be iterable
         try:
             for item in what: self.logf.write(str(item))
             self.logf.write("\n")
-        except: print("ERROR: Logging: log file not present or called incorrectly",str(what))
+        except:
+            print("ERROR: Logging: log file not present or called incorrectly", str(what))
+
 
 if __name__ == "__main__":
     print("This is a module file, run natlan.py instead")
-        
-
--- natlan.py
+++ natlan.py
@@ -1,28 +1,27 @@
 import sys, gl, conc, wrd, testing
 
-
-gl.args=gl.Arguments()          #initialize
-gl.WM = conc.Kbase("WM")        # WORKING MEMORY
-gl.KB = conc.Kbase("KB")        # KNOWLEDGE BASE
-gl.WL = wrd.Wlist("WL")         # WORD LIST
+gl.args = gl.Arguments()  # initialize
+gl.WM = conc.Kbase("WM")  # WORKING MEMORY
+gl.KB = conc.Kbase("KB")  # KNOWLEDGE BASE
+gl.WL = wrd.Wlist("WL")  # WORD LIST
 gl.log = gl.Logging()
 
-gl.KB.add_concept(3,3,[9,5])
+gl.KB.add_concept(3, 3, [9, 5])
 gl.WL.add_word("pista")
-gl.WM.add_concept(3,1,[19])
-gl.WM.add_concept(4,2,[12,17])
+gl.WM.add_concept(3, 1, [19])
+gl.WM.add_concept(4, 2, [12, 17])
 gl.WL.add_word("joska")
 
-test1=conc.Concept()
-test1.add_parents([-1,17])
-result=gl.WM.search_inlist(test1)
+test1 = conc.Concept()
+test1.add_parents([-1, 17])
+result = gl.WM.search_inlist(test1)
 
-if gl.args.argnum==2:
-    gl.test=testing.Testinput(sys.argv[1])
+if gl.args.argnum == 2:
+    gl.test = testing.Testinput(sys.argv[1])
     gl.test.readtest()
-    print ("input file read test - row 3 eng:",gl.test.eng[2],"row 5 mentalese:",gl.test.mentalese[4],"goodanswer:",gl.test.goodanswer[4])
+    print("input file read test - row 3 eng:", gl.test.eng[2], "row 5 mentalese:", gl.test.mentalese[4], "goodanswer:",
+          gl.test.goodanswer[4])
     gl.test.testf.close()
 
 gl.log.logf.close()
-print (gl.KB.cp[0].parent,"word with index 1=",gl.WL.wcp[1].word,"search:",result)
-		
+print(gl.KB.cp[0].parent, "word with index 1=", gl.WL.wcp[1].word, "search:", result)
